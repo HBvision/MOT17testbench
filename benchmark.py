ecf123ea45e044443b67f836f6f977b13354ffed
ianhaggerty3 <ianhaggerty3@gmail.com>
2019-02-24 05:05:12 -0500

    Fixed the code so that it works with for loops; there were some bugs before. At this point, the core functionality works as intended, I just need to figure out the finer points of using motmetrics, which should be much easier; this code is ready for use as general io handling for everyone.

--- benchmark.py
+++ benchmark.py
@@ -1,7 +1,6 @@
-import pandas as pd
 import cv2
-import motmetrics as mm
 import glob
+import motmetrics as mm
 import numpy as np
 
 
@@ -28,15 +27,26 @@ class DataLoader:
         """
 
         self.sequence = 1
-        return self
+        error_check = False
+        while not error_check:
+            try:
+                current_sequence = self.load_sequence(self.sequence)
+                self.sequence += 1
+                yield current_sequence
+            except IOError:
+                error_check = True
+        # return self
 
     def __next__(self):
-        try:
-            sequence = self.load_sequence(self.sequence)
-            self.sequence += 1
-            return sequence
-        except IOError:
-            raise StopIteration
+        error_check = False
+        while not error_check:
+            try:
+                current_sequence = self.load_sequence(self.sequence)
+                self.sequence += 1
+                yield current_sequence
+            except IOError:
+                error_check = False
+                raise StopIteration
 
     def load_sequence(self, sequence_number):
         """
@@ -48,13 +58,12 @@ class DataLoader:
 
         if len(str(sequence_number)) == 1:
             sequence_string = '0' + str(sequence_number)
-        data_path = ''.join(glob.glob(self.filepath + 'data/*/MOT16-' + sequence_string, recursive=True))
+        else:
+            sequence_string = str(sequence_number)
+        sequence_filepath = self.filepath + 'data/*/MOT16-' + sequence_string
+        data_path = ''.join(glob.glob(sequence_filepath, recursive=False))
         if len(data_path) == 0:
             raise IOError  # Raise an error if the sequence cannot be found
-        if 'train' in data_path:
-            data_path += '/gt/gt.txt'  # If we are dealing with the training set, we should use the ground truth file
-        else:
-            data_path += '/det/det.txt'  # If we are dealing with the testing set, there is no ground truth file
 
         return SequenceLoader(filepath=data_path)
 
@@ -69,46 +78,54 @@ class SequenceLoader():
         self.filepath = filepath
         with open(self.filepath + '/seqinfo.ini') as fid:
             info = fid.readlines()
-        self.name =          info[1].split('=')[1]
-        self.imDir =         info[2].split('=')[1]
-        self.frameRate =     info[3].split('=')[1]
-        self.seqLength =     info[4].split('=')[1]
-        self.imWidth =       info[5].split('=')[1]
-        self.imHeight =      info[6].split('=')[1]
-        self.imExt =         info[7].split('=')[1]
+        self.name =          info[1].split('=')[1][:-1]
+        self.imDir =         info[2].split('=')[1][:-1]
+        self.frameRate =     info[3].split('=')[1][:-1]
+        self.seqLength =     info[4].split('=')[1][:-1]
+        self.imWidth =       info[5].split('=')[1][:-1]
+        self.imHeight =      info[6].split('=')[1][:-1]
+        self.imExt =         info[7].split('=')[1][:-1]
 
 
     def __iter__(self):
-        """
-        Defines the behavior of the iterator function for this object
-
-        :return: This object as an iterator, allowing the user to iterate through each frame's data
-        """
-
+        # return self
+        generator = self.load_gt()
         self.frame = 1
-        self.generator = self.load_gt()
-        return self
+        for frame in range(1, int(self.seqLength) + 1):
+            frame_string = str(frame).zfill(6)
+            img_filename = self.filepath + '/' + self.imDir + '/' + frame_string + self.imExt
+            img = cv2.imread(img_filename)
+            if img is None:
+                break
+            yield img, next(generator)
 
     def __next__(self):
-        '''
+        """
         Defines generator functionality to the SequenceLoader object by returning the next frame in the sequnece as well
         as information about its ground truth bounding boxes
 
         :return: A numpy array of the image followed by a list of ground truth detections for a given frame in the
         correct format
-        '''
-        frame_string = str(self.frame).zfill(6)
-        img = cv2.imread(self.filepath + '/' + self.imDir + '/' + frame_string + self.imExt)
-        if img is None:
-            raise StopIteration # Terminates the generator
-        return img, next(generator)
+        """
+        generator = self.load_gt()
+        for frame in range(1, int(self.seqLength) + 1):
+            frame_string = str(frame).zfill(6)
+            img = cv2.imread(self.filepath + '/' + self.imDir + '/' + frame_string + self.imExt)
+            if img is None:
+                break
+            yield img, next(generator)
 
     def load_gt(self):
-        with open(fname, 'r') as fid:
+        if 'train' in self.filepath:
+            gt_filepath = self.filepath + '/gt/gt.txt'  # If we are dealing with the training set, we should use the ground truth file
+        else:
+            gt_filepath = self.filepath + '/det/det.txt'  # If we are dealing with the testing set, there is no ground truth file
+        with open(gt_filepath, 'r') as fid:
             boxes = []  # A list to store the data on each bounding box
             for line in fid:
                 box = line.split(',')
                 if int(line[0]) != self.frame:
+                    self.frame += 1
                     yield (boxes)
                     del (boxes[:])
                 boxes.append([float(box[x]) for x in range(2, 7)])  # Add the relevant parts of the box info
@@ -129,17 +146,19 @@ if __name__ == '__main__':
     loader = DataLoader()
     acc = mm.MOTAccumulator(auto_id=True)
     for sequence in loader:
-        for frame, data in sequence:
-            coords = np.array(data)
+        for frame, gt_data in sequence:
+            coords = np.array(gt_data)
             dists = mm.distances.norm2squared_matrix(coords, coords)
             acc.update(
-                list(range(len(frame))),
-                list(range(len(frame))),
-                dists
+               list(range(len(frame))),
+               list(range(len(frame))),
+               dists
             )
 
 
     # Now that the accumulator has been sufficiently
-    mh = mm.metrics.create()
-    summary = mh.compute(acc, metrics=['num_frames', 'mota', 'motp'])
-    print(summary)
+    # mh = mm.metrics.create()
+    # summary = mh.compute(acc, metrics=['num_frames', 'mota', 'motp'])
+    # print(summary)
+
+    print('yeet')
