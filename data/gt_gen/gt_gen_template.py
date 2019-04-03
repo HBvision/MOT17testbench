9daaa795fe01bfef672878feb0cfbd23061d806d
Ian Haggerty <ianhaggerty3@gmail.com>
2019-04-03 12:42:50 -0400

    Restructured more of the dataset folder and added a rough gt for sequence 2

---
+++ data/gt_gen/gt_gen_template.py
@@ -0,0 +1,38 @@
+# Example gt filename
+seq_filename = "../HB/HB02/seqinfo.ini"
+gt_filename = "../HB/HB02/gt/gt.txt"
+
+starting_frame = 80
+x_pos = 0
+y_pos = float(1080 / 2 + 100)
+height = 1080 * 2 / 5
+width = 130
+
+fid = open(gt_filename, "w")
+x_speed = 8
+y_speed = 0.3
+for x in range(1, starting_frame):
+    fid.write(f"{x},3,{0},{0},{0},{0},-1,-1,-1,-1,-1\n")
+
+x_speed = 8
+for x in range(starting_frame, 337, 1):
+    fid.write(f"{x},1,{int(x_pos)},{int(y_pos)},{width},{height},-1,-1,-1,-1,-1\n")
+    if x == 180:
+        x_speed = 14
+
+    x_pos += x_speed
+    y_pos -= y_speed
+
+x_speed = 12
+x_pos = 1650
+y_pos = 1080 / 2 + 20
+for x in range(starting_frame, 337, 1):
+    if x == 120:
+        x_speed = 8
+    if x == 180:
+        x_speed = 7
+    fid.write(f"{x},2,{x_pos},{y_pos},{width},{height},-1,-1,-1,-1,-1\n")
+    x_pos -= x_speed
+    y_pos += y_speed
+
+fid.close()
--- show_gt.py
+++ show_gt.py
@@ -5,14 +5,30 @@ dataset_path = './data/' + "HB"
 #dataset_path = './data/' + input("Please input name of the dataset: ")
 #dataset_path = root + '/' + input("Please input the name of the dataset: ")
 
-loader = benchmark.DataLoader(dataset_path)
-
-for sequence in loader:
-    for frame, gt_data in sequence:
+sequence = benchmark.SequenceLoader('./data/HB/HB02')
+frame_num = 1
+for frame, gt_data in sequence:
+    if frame_num >= 80:
         for box in gt_data:
             int_box = [int(x) for x in box]
-            cv2.rectangle(frame, (int_box[0], int_box[1]), (int_box[0] + int_box[2], int_box[1] + int_box[3]), (0, 255, 0), 3)
-        cv2.imshow('image', frame)
-        cv2.waitKey(1)
+            cv2.rectangle(frame, (int_box[0], int_box[1]), (int_box[0] + int_box[2], int_box[1] + int_box[3]), (0, 255, 0),
+                          3)
+    if frame_num == 180:
+        print('yeet')
+        pass
+    new_frame = cv2.resize(frame, (1366, 768))
+    cv2.imshow('image', new_frame)
+    cv2.waitKey(1)
+    frame_num += 1
+
+# loader = benchmark.DataLoader(dataset_path)
+#
+# for sequence in loader:
+#     for frame, gt_data in sequence:
+#         for box in gt_data:
+#             int_box = [int(x) for x in box]
+#             cv2.rectangle(frame, (int_box[0], int_box[1]), (int_box[0] + int_box[2], int_box[1] + int_box[3]), (0, 255, 0), 3)
+#         cv2.imshow('image', frame)
+#         cv2.waitKey(1)
 
 cv2.destroyAllWindows()
