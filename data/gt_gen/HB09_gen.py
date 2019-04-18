fbca348dab4443d4567b92f5bcd9208563141434
Ian Haggerty <ianhaggerty3@gmail.com>
2019-04-18 18:48:05 -0400

    Added two of the three correct bounding boxes for HB09

---
+++ data/gt_gen/HB09_gen.py
@@ -0,0 +1,122 @@
+import benchmark
+
+# Example gt filename
+root = "../HB/HB09/"
+seq_filename = root + "seqinfo.ini"
+gt_filename = root + "/gt/gt.txt"
+
+sequence_info = benchmark.SequenceLoader(root)
+fid = open(gt_filename, "w")
+
+img_width = int(sequence_info.imWidth) # 1920 for this dataset
+img_height = int(sequence_info.imHeight) # 1080 for this dataset
+
+# First bounding box (Sumati)
+starting_frame = 1
+x_pos = img_width / 2 - 340
+x_speed = 0
+y_pos = float(img_height / 4 + 50)
+y_speed = 0.5
+height = float(img_height / 2)
+height_speed = 1
+width = 220
+width_speed = 0
+
+# Sequence gets way too close at frame 190
+ending_frame = 190
+
+for x in range(starting_frame, ending_frame, 1):
+    fid.write(f"{x},1,{int(x_pos)},{int(y_pos)},{width},{int(height)},-1,-1,-1,-1,-1\n")
+    if x == 40:
+        x_speed = -2
+        width_speed = 2
+        y_speed = 0.8
+        height_speed = 4
+    if x == 80:
+        x_speed = -4
+        width_speed = 3
+        y_speed = 1.4
+        height_speed = 8
+    if x == 120:
+        x_speed = -4
+        width_speed = -1
+        y_speed = 3
+    if x == 150:
+        x_speed = -8
+        width_speed = 4
+
+    if y_pos + height > img_height:
+        height_speed = y_speed
+
+    x_pos += x_speed
+    y_pos -= y_speed
+    height += height_speed
+    width += width_speed
+
+# Second bounding box (Rohan)
+starting_frame = 1
+x_pos = img_width / 2 - 100
+x_speed = 0
+y_pos = float(img_height / 4 + 30)
+y_speed = 0.5
+height = float(img_height / 2) - 50
+height_speed = 1
+width = 160
+width_speed = 0
+
+for x in range(starting_frame, int(sequence_info.seqLength), 1):
+    fid.write(f"{x},1,{int(x_pos)},{int(y_pos)},{width},{int(height)},-1,-1,-1,-1,-1\n")
+    if x == 30:
+        y_speed = 0.6
+        height_speed = 3
+        x_speed = -0.3
+        width_speed = 1
+    if x == 60:
+        width_speed = 2
+    if x == 90:
+        x_speed = -1
+        height_speed = 5
+    if x == 120:
+        y_speed = 2
+    if x == 180:
+        x_speed = 2
+        y_speed = 4
+        width_speed = 4
+    if x == 200:
+        x_speed = 12
+        width_speed = 0
+        y_speed = 0
+
+    if y_pos - 10 < 0:
+        y_speed = 0
+
+    if y_pos + height > img_height:
+        height_speed = y_speed
+
+    x_pos += x_speed
+    y_pos -= y_speed
+    height += height_speed
+    width += width_speed
+
+# Third bounding box ()
+starting_frame = 1
+x_pos = img_width / 2 + 100
+x_speed = 0
+y_pos = float(img_height / 4) - 25
+y_speed = 0.5
+height = float(img_height / 2) + 100
+height_speed = 1
+width = 160
+width_speed = 0
+
+for x in range(starting_frame, int(sequence_info.seqLength), 1):
+    fid.write(f"{x},1,{int(x_pos)},{int(y_pos)},{width},{int(height)},-1,-1,-1,-1,-1\n")
+    if x == 30:
+        x_speed = 5
+
+    x_pos += x_speed
+    y_pos -= y_speed
+    height += height_speed
+    width += width_speed
+
+fid.close()
--- show_gt.py
+++ show_gt.py
@@ -5,7 +5,7 @@ dataset_path = './data/' + "HB"
 # dataset_path = './data/' + input("Please input name of the dataset: ")
 # dataset_path = root + '/' + input("Please input the name of the dataset: ")
 
-sequence = benchmark.SequenceLoader(dataset_path + '/HB08')
+sequence = benchmark.SequenceLoader(dataset_path + '/HB09')
 
 frame_num = 1
 count = 1
@@ -13,14 +13,17 @@ for frame, gt_data in sequence:
     for box in gt_data:
         int_box = [int(x) for x in box]
         if count == 1:
-            cv2.rectangle(frame, (int_box[0], int_box[1]), (int_box[0] + int_box[2], int_box[1] + int_box[3]), (0, 255, 0),
-                      3)
+            cv2.rectangle(frame, (int_box[0], int_box[1]), (int_box[0] + int_box[2], int_box[1] + int_box[3]),
+                          (0, 255, 0), 3)
             count += 1
         elif count == 2:
             cv2.rectangle(frame, (int_box[0], int_box[1]), (int_box[0] + int_box[2], int_box[1] + int_box[3]),
-                          (0, 0, 255),
-                          3)
+                          (0, 0, 255), 3)
             count += 1
+        elif count == 3:
+            cv2.rectangle(frame, (int_box[0], int_box[1]), (int_box[0] + int_box[2], int_box[1] + int_box[3]),
+                          (255, 0, 0), 3)
+
     new_frame = cv2.resize(frame, (int(1366 / 2), int(768 / 2)))
     # new_frame = frame
     cv2.putText(new_frame, f'{frame_num}', (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
